7c63a6d140fd9676c99204987a78094580e9f5b0
Arnav Bansal <arnavbansal101@gmail.com>
2025-06-28 01:15:17 +0530

    nvim: setup formatting

--- nvim/.config/nvim/lua/config/plugins/conform.lua
+++ nvim/.config/nvim/lua/config/plugins/conform.lua
@@ -3,12 +3,26 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
+        lua = { "stylua" },
         go = { "gofmt" },
+        javascript = { "biome", "biome-organize-imports", "prettier" },
+        typescript = { "biome", "biome-organize-imports", "prettier" },
       },
       -- format_on_save = {
       --   timeout_ms = 500,
       --   lsp_format = "fallback"
       -- }
+      formatters = {
+        prettier = {
+          require_cwd = true
+        },
+        biome = {
+          require_cwd = true
+        },
+        ["biome-organize-imports"] = {
+          require_cwd = true
+        },
+      },
     })
   end
 }
